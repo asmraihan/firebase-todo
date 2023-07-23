eb5b606554e076d4a4af70d98ea059d27daf5dbf
asmraihan <asmraihanctg14@gmail.com>
2023-07-24 02:33:42 +0600

    login

--- App.tsx
+++ App.tsx
@@ -4,14 +4,15 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import List from './app/screens/List';
 import Details from './app/screens/Details';
+import Login from './app/screens/Login';
 
 const Stack = createNativeStackNavigator()
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName='Login'>
         <Stack.Screen name="My Todos" component={List} />
-        <Stack.Screen name="Details" component={Details} />
+        <Stack.Screen name="Login" component={Login} />
       </Stack.Navigator>
       {/* <View className="flex w-full h-full justify-center items-center bg-slate-900">
         <Text className='text-lg text-white font-semibold'>Open up App.tsx to start working on your app!</Text>
---
+++ app/screens/Login.tsx
@@ -0,0 +1,39 @@
+import { View, Text, Button, TextInput, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+
+const Login = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const signUp = async () => {
+
+    }
+    const signIn = async () => { }
+
+    return (
+        <View className='flex flex-col justify-center items-center w-full h-full'>
+            <View className='w-11/12 mx-auto mb-2 '>
+                {/* <Text className='text-2xl font-semibold text-center mb-4'>Please Login</Text> */}
+                <Text className='p-2'>Enter your email address</Text>
+                <TextInput className='p-2 border-gray-300 border-2 mb-2 rounded-md px-4 hover:border-blue-500' placeholder='Email'
+                    value={email}
+                    onChangeText={(text: string) => setEmail(text)}
+                />
+                <Text className='p-2'>Enter your password</Text>
+                <TextInput className='p-2 border-gray-300 border-2 mb-2 rounded-md px-4 hover:border-blue-500' placeholder='Password'
+                    textContentType='password'
+                    value={password}
+                    onChangeText={(text: string) => setPassword(text)}
+                />
+                <TouchableOpacity className="bg-orange-500 p-3 rounded-lg mt-4" onPress={signUp}>
+                    <Text className='text-center text-white font-semibold '>Create account</Text>
+                </TouchableOpacity>
+                <TouchableOpacity className="bg-blue-500 p-3 rounded-lg mt-4" onPress={signIn}>
+                    <Text className='text-center text-white font-semibold '>Sign in</Text>
+                </TouchableOpacity>
+
+            </View>
+        </View>
+    )
+}
+
+export default Login
