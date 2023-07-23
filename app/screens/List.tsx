2a5aede92c83cbb0589d5d564c54c6c448c6e653
asmraihan <asmraihanctg14@gmail.com>
2023-07-24 00:26:17 +0600

    sub collection

--- app/screens/List.tsx
+++ app/screens/List.tsx
@@ -1,15 +1,30 @@
 import { View, Text, Button, TextInput, TouchableOpacity, Pressable } from 'react-native'
 import React, {useEffect, useState} from 'react'
 import { FIRESTORE_DB } from '../../firebaseConfig'
-import { addDoc, collection } from 'firebase/firestore'
+import { addDoc, collection, doc, onSnapshot } from 'firebase/firestore'
 
 const List = ({ navigation }: any) => {
 
     const [todos, setTodos] = useState<any[]>([])
     const [todo, setTodo] = useState('')
-    // useEffect(() => {
+    // get all todos from firestore
+    useEffect(() => {
+        const todoRef = collection(FIRESTORE_DB, 'todos')
+        const subscriber = onSnapshot(todoRef, {
+            next: (snapshot) => {
+                const todos: any[] = []
+                snapshot.docs.forEach(doc =>{
+                    todos.push({
+                        id: doc.id,
+                        ...doc.data(),
+                    })
+                })
+                setTodos(todos)
+            }
+        })
 
-    // }, [])
+        return () => subscriber()
+    }, [])
     
     const addTodo = async() => {
         const doc = await addDoc(collection(FIRESTORE_DB, 'todos'),{ title : todo, done: false} )
@@ -25,6 +40,14 @@ const List = ({ navigation }: any) => {
                 value={todo}
                 /> 
                  <Button onPress={()=>addTodo()} title='Add Todo' disabled={todo === ''}/>
+            </View>
+            <View>
+                {
+                    todos.map(todo => (
+                        <Text key={todo.id}>{todo.title}</Text>
+                    ))
+
+                }
             </View>
             {/*
             <Button onPress={() => navigation.navigate('Details')} title="Open Details"></Button> */}
