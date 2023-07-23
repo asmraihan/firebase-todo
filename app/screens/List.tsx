fa391ac216a9095eaf4b3a1ff6ef7a22f953dacc
asmraihan <asmraihanctg14@gmail.com>
2023-07-24 02:12:57 +0600

    collection update/delete

--- app/screens/List.tsx
+++ app/screens/List.tsx
@@ -1,23 +1,28 @@
-import { View, Text, Button, TextInput, TouchableOpacity, Pressable } from 'react-native'
-import React, {useEffect, useState} from 'react'
+import { View, Text, Button, TextInput, TouchableOpacity, Pressable, FlatList } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { FIRESTORE_DB } from '../../firebaseConfig'
-import { addDoc, collection, doc, onSnapshot } from 'firebase/firestore'
+import { addDoc, collection, deleteDoc, doc, onSnapshot, updateDoc } from 'firebase/firestore'
+import Icon from 'react-native-vector-icons/Octicons';
+interface Todo {
+    title: string,
+    done: boolean,
+    id: string,
+}
 
 const List = ({ navigation }: any) => {
-
-    const [todos, setTodos] = useState<any[]>([])
+    const [todos, setTodos] = useState<Todo[]>([])
     const [todo, setTodo] = useState('')
     // get all todos from firestore
     useEffect(() => {
         const todoRef = collection(FIRESTORE_DB, 'todos')
         const subscriber = onSnapshot(todoRef, {
             next: (snapshot) => {
-                const todos: any[] = []
-                snapshot.docs.forEach(doc =>{
+                const todos: Todo[] = []
+                snapshot.docs.forEach(doc => {
                     todos.push({
                         id: doc.id,
                         ...doc.data(),
-                    })
+                    } as Todo)
                 })
                 setTodos(todos)
             }
@@ -25,30 +30,52 @@ const List = ({ navigation }: any) => {
 
         return () => subscriber()
     }, [])
-    
-    const addTodo = async() => {
-        const doc = await addDoc(collection(FIRESTORE_DB, 'todos'),{ title : todo, done: false} )
+
+    const addTodo = async () => {
+        const doc = await addDoc(collection(FIRESTORE_DB, 'todos'), { title: todo, done: false })
         console.log(doc)
         setTodo('')
     }
-    
+
+    const renderTodo = ({ item }: { item: Todo}) => {
+        const ref = doc(FIRESTORE_DB, `todos/${item.id}`)
+        const toggleDone = async () => {
+            updateDoc(ref, {done: !item.done})
+        }
+        const deleteItem = async () => {
+            deleteDoc(ref)
+        }
+        return (
+            <View className='flex flex-row justify-between items-center mx-4 my-2'>
+                <TouchableOpacity className='w-8/12' onPress={toggleDone} >
+                    <Text className={`text-lg ${item.done ? 'line-through' : ''}`}>{ !item.done && '🔲'}{ item.done && '✅'}  {item.title}</Text>
+                </TouchableOpacity>
+                <TouchableOpacity className="bg-red-500 p-2 rounded-lg" onPress={deleteItem}>
+                    <Icon  name="x-circle" size={20} color="#ffffff" />
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
     return (
         <View>
-            <View className='w-11/12 mx-auto'>
-                <TextInput className='p-2 border-gray-300 border-2 my-2 rounded-md px-4 hover:border-blue-500' placeholder='Add new todo'
-                onChangeText={(text : string) => setTodo (text)}
-                value={todo}
-                /> 
-                 <Button onPress={()=>addTodo()} title='Add Todo' disabled={todo === ''}/>
+            <View className='w-11/12 mx-auto mb-2'>
+                <TextInput className='p-2 border-gray-300 border-2 my-4 rounded-md px-4 hover:border-blue-500' placeholder='Add new todo'
+                    onChangeText={(text: string) => setTodo(text)}
+                    value={todo}
+                />
+                <Button onPress={() => addTodo()} title='Add Todo' disabled={todo === ''} />
             </View>
-            <View>
-                {
-                    todos.map(todo => (
-                        <Text key={todo.id}>{todo.title}</Text>
-                    ))
+            {todos.length > 0 && (
+                <View className='w-11/12 mx-auto'>
+                  <FlatList 
+                  data={todos}
+                    renderItem={renderTodo} /* FIX */
+                    keyExtractor={(todo: Todo) => todo.id}
+                  />
+                </View>
+            )}
 
-                }
-            </View>
             {/*
             <Button onPress={() => navigation.navigate('Details')} title="Open Details"></Button> */}
         </View>
